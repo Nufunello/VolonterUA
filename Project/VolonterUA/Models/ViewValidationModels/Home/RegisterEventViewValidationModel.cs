fcaa19eead69ad789e32bad92a5ea35582692a42
Nufunello <nafania578@gmail.com>
2020-12-02 22:10:52 +0200

    Added adding of event

---
+++ Project/VolonterUA/Models/ViewValidationModels/Home/RegisterEventViewValidationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VolonterUA.Models.Database;
+
+namespace VolonterUA.Models.ViewValidationModels.Home
+{
+    public class RegisterEventViewValidationModel
+    {
+        [Required]
+        public VolonterEvent VolonterEvent { get; set; }
+    }
+}
